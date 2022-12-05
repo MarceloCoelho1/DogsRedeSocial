bcaf835a9bb947481ca193b2db36d65221e854c6
Marcelo Coelho <79272572+MarceloCoelho1@users.noreply.github.com>
2022-12-05 10:41:59 -0300

    Add files via upload

---
+++ dogs/build/precache-manifest.d12d42c6396878269debdc27ff278ded.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de2640f14788bb8dbee4d3b92bb0c6bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ae7a630744ffa7dc96d",
+    "url": "/static/css/4.39e776f4.chunk.css"
+  },
+  {
+    "revision": "3739e6c271e2ad7a45f7",
+    "url": "/static/css/main.35b00bdc.chunk.css"
+  },
+  {
+    "revision": "789a4b4264daece4ff8c",
+    "url": "/static/js/2.8e16a9c9.chunk.js"
+  },
+  {
+    "revision": "e171f8b16cc4abdaa333",
+    "url": "/static/js/3.3a054638.chunk.js"
+  },
+  {
+    "revision": "b09a1c16cd7c7eba5171a86d359e37fb",
+    "url": "/static/js/3.3a054638.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ae7a630744ffa7dc96d",
+    "url": "/static/js/4.8719d564.chunk.js"
+  },
+  {
+    "revision": "3739e6c271e2ad7a45f7",
+    "url": "/static/js/main.f418ce58.chunk.js"
+  },
+  {
+    "revision": "dc38fa34c9fc5de66367",
+    "url": "/static/js/runtime-main.72b32d9f.js"
+  },
+  {
+    "revision": "c6e243c75ebc97ddc04eb362ec882b0b",
+    "url": "/static/media/adicionar.c6e243c7.svg"
+  },
+  {
+    "revision": "220b6e5ec18637fb70ff2a6837f81939",
+    "url": "/static/media/dogs-footer.220b6e5e.svg"
+  },
+  {
+    "revision": "745f4ff4e528ca4141ed9adf1d6d46af",
+    "url": "/static/media/dogs.745f4ff4.svg"
+  },
+  {
+    "revision": "a668a9cce590cadcacc5ab4117cd2ed3",
+    "url": "/static/media/enviar.a668a9cc.svg"
+  },
+  {
+    "revision": "5ce6ac31ceefce525e7de75fb3298f1a",
+    "url": "/static/media/estatisticas.5ce6ac31.svg"
+  },
+  {
+    "revision": "95db747853a588581330f83bd79bea07",
+    "url": "/static/media/feed.95db7478.svg"
+  },
+  {
+    "revision": "824d7d85ae3af9a43a61d46fe174192e",
+    "url": "/static/media/login.824d7d85.jpg"
+  },
+  {
+    "revision": "efa03022cd66ca19df390c3ffdc421ec",
+    "url": "/static/media/sair.efa03022.svg"
+  },
+  {
+    "revision": "78901e1fff162801f63d4edba671cbad",
+    "url": "/static/media/usuario.78901e1f.svg"
+  },
+  {
+    "revision": "9b449f44a776eda3d00c1c5e0c92da85",
+    "url": "/static/media/visualizacao-black.9b449f44.svg"
+  },
+  {
+    "revision": "b23c59ac49db2d8dd3298c68100fea31",
+    "url": "/static/media/visualizacao.b23c59ac.svg"
+  }
+]);
